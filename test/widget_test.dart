d7b5979eb27bd3e1f5a31e377b20510d47e2a7ec
shaojunhui <shaojunhui@foundersc.com>
2019-04-04 13:24:03 +0800

    add base test via cirrus

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -16,15 +16,17 @@ void main() {
     await tester.pumpWidget(MainPage());
 
     // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    expect(find.text('行情'), findsOneWidget);
 
     // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
+    await tester.tap(find.text('行情'));
     await tester.pump();
 
     // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    expect(find.text('Item 0'), findsOneWidget);
+    expect(find.text('Item 1'), findsOneWidget);
+
+    await tester.tap(find.text('Item 2'));
+    await tester.pump();
   });
 }
