f859b0bdb2e04e31cf18ffd28910dd2fe7d20699
Dozen Lue <dozenlue@qq.com>
2019-04-20 09:38:29 +0800

    A simple profile page (#18)
    * A simple profile page
    
    * format code

--- lib/main/pages/main_mine_page.dart
+++ lib/main/pages/main_mine_page.dart
@@ -8,44 +8,127 @@ class MinePage extends StatefulWidget {
 class _MinePageState extends State<MinePage>
     with AutomaticKeepAliveClientMixin {
   @override
-  // TODO: implement wantKeepAlive
   bool get wantKeepAlive => true;
-
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: PreferredSize(
-          child: AppBar(
-            title: Text('我的'),
-          ),
-          preferredSize: Size.fromHeight(48.0)),
-      body: new Center(
-        child: new Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            new Text(
-              'You have pushed the button this many times:',
-            ),
-            new Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
+        appBar: AppBar(
+          title: Text('我的'),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(
+                Icons.settings,
+                size: 32,
+              ),
+              onPressed: () {
+                debugPrint("Settings button pressed");
+              },
+            )
           ],
         ),
-      ),
-      floatingActionButton: new FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: new Icon(Icons.add),
-      ),
-    );
+        body: new ListView(children: <Widget>[
+          Card(
+            child: Row(
+              children: <Widget>[
+                Icon(Icons.person, size: 128),
+                Text("我的名字"),
+              ],
+            ),
+          ),
+          Card(
+            child: SizedBox.fromSize(
+              size: Size.fromHeight(64.0),
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                // padding: EdgeInsets.all(6.0),
+                itemExtent: 128.0,
+                children: <Widget>[
+                  Container(
+                    color: Theme.of(context).backgroundColor,
+                    padding: EdgeInsets.all(6.0),
+                    child: RaisedButton(
+                      elevation: 5.0,
+                      child: Text("我的组合"),
+                      color: Colors.black38,
+                      textColor: Colors.white,
+                      onPressed: () {
+                        debugPrint("Button Pressed");
+                      },
+                    ),
+                  ),
+                  Container(
+                    color: Theme.of(context).backgroundColor,
+                    padding: EdgeInsets.all(6.0),
+                    child: RaisedButton(
+                      elevation: 5.0,
+                      child: Text("我的投顾"),
+                      color: Colors.black38,
+                      textColor: Colors.white,
+                      onPressed: () {
+                        debugPrint("Button Pressed");
+                      },
+                    ),
+                  ),
+                  Container(
+                    color: Theme.of(context).backgroundColor,
+                    padding: EdgeInsets.all(6.0),
+                    child: RaisedButton(
+                      elevation: 5.0,
+                      child: Text("我的理财"),
+                      color: Colors.black38,
+                      textColor: Colors.white,
+                      onPressed: () {
+                        debugPrint("Button Pressed");
+                      },
+                    ),
+                  ),
+                  Container(
+                    color: Theme.of(context).backgroundColor,
+                    padding: EdgeInsets.all(6.0),
+                    child: RaisedButton(
+                      elevation: 5.0,
+                      child: Text("我的特权"),
+                      color: Colors.black38,
+                      textColor: Colors.white,
+                      onPressed: () {
+                        debugPrint("Button Pressed");
+                      },
+                    ),
+                  ),
+                  Container(
+                    color: Theme.of(context).backgroundColor,
+                    padding: EdgeInsets.all(6.0),
+                    child: RaisedButton(
+                      elevation: 5.0,
+                      child: Text("我的不知道还有点儿啥"),
+                      color: Colors.black38,
+                      textColor: Colors.white,
+                      onPressed: () {
+                        debugPrint("Button Pressed");
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          ListTile(
+            title: Text("我的收藏"),
+            trailing: Icon(Icons.arrow_right),
+          ),
+          ListTile(
+            title: Text("我的消息"),
+            trailing: Icon(Icons.arrow_right),
+          ),
+          ListTile(
+            title: Text("我的服务"),
+            trailing: Icon(Icons.arrow_right),
+          ),
+          ListTile(
+            title: Text("邀请朋友"),
+            trailing: Icon(Icons.arrow_right),
+          ),
+        ]));
   }
 }
